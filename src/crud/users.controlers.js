0d59a13ea55c990ecf407af72addb9684bdf65b6
Supremoestelar369 <111463579+Supremoestelar369@users.noreply.github.com>
2022-11-23 20:56:07 -0600

    Update and rename crud.controller.js to users.controlers.js

---
+++ src/crud/users.controlers.js
@@ -0,0 +1,45 @@
+const usersDB = []
+let id =1;
+
+const findAllUser = () => {
+    return usersDB
+}
+const findUsersById = (id) => {
+const filteredUser = usersDB.find( (user) => user.id == id)
+    return filteredUser
+}
+const createNewUsers = (obj) => {
+    const newUser = {
+        id: id++,
+        first_name:obj.first_name , 
+        last_name: obj.last_name ,
+        email:obj.email || 'Email Unknow' , 
+        password: obj.password,
+        birthday: obj.birthday
+    }
+    usersDB.push(newUser)
+    return newUser
+}
+const FindRandomUsers = () => {
+    const randomIndex = Math.floor(Math.random() * usersDB.length)
+    return usersDB[randomIndex]
+}
+//deleated
+const DeleteOneUser = (id) =>{
+    const userindex = usersDB.findIndex( (user)=>user.id ==id )
+   if (userindex !== -1) {
+    usersDB.slice(userindex,1)
+    return true
+   }
+   return false
+}
+
+
+// exportar las funcioness
+module.exports={
+    findAllUser,
+    findUsersById,
+    createNewUsers,
+    FindRandomUsers,
+    DeleteOneUser
+}
